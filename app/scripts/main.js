20d11004e99f9918d7ebf15a88d00ec2c779f4c8
Hachimi <mohamed.rez@gmail.com>
2016-09-06 18:07:59 +0100

    fixed bunch of stuffs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,7 @@
 (function($){
 
 	var selected_date = {};
+	var busy_dates = [];
   $('#calendar').fullCalendar({
 		weekends: false,
 		defaultView: 'basicWeek',
@@ -8,10 +9,26 @@
 			return false;
     },
    	dayClick: function (date, jsEvent, view) {
-      // $(jsEvent.target).toggleClass("fc-state-highlight");
-   		set_selected_date(date);
+ 			set_selected_date(date);
+   		if(!check_availability()){
+   			selected_date = {};
+   			show_error_modal("L'equipe n'est pas disponible");
+   		}
    	} 
   });
+
+	function check_availability(){
+  	if(!selected_date.start) return true;
+  	if(!selected_date.end) return true;
+  	if(!busy_dates.length) return true;
+  	var is_available = true;
+		$.each(busy_dates, function(index, date) {
+  		if(moment(date).isBetween(selected_date.start,selected_date.end, 'days','[]')){
+  			is_available = false;
+  		}
+		});
+		return is_available;
+	}
 
   function set_selected_date(date){
   	if(!selected_date.start){
@@ -27,23 +44,28 @@
   	}
   }
 
-  function highlight_selected_date(){
-  	
+  function highlight_dates(){
   	$('.fc-day').each(function(){
   		var $this = $(this);
   		var calendar_date = moment($this.data('date'));
-  		if(calendar_date.isBetween(selected_date.start, selected_date.end, 'days','[]')){
+  		if(selected_date.start && calendar_date.isBetween(selected_date.start, selected_date.end, 'days','[]')){
   			$this.addClass('fc-selected');
   		}else{
   			$this.removeClass('fc-selected');
   		}
+  		if(busy_dates.length){
+	  		if(busy_dates.includes($this.data('date'))){
+  				$this.addClass('fc-busy');
+  			}
+  		}
   	});
   }
 
-  var colorise = setInterval(highlight_selected_date, 300);
+  var colorise = setInterval(highlight_dates, 300);
   
   $('#teams').on('change',function(event) {
   	var $this = $(this);
+  	busy_dates = [];
   	get_team_schedule($this.val());
   });
 
@@ -52,15 +74,38 @@
   		url: 'http://localhost:3000/schedules',
   		data: {team_id: id},
   	})
-  	.done(display_busy_dates)
-  	.fail(function() {
-  		console.log("error");
+  	.done(set_busy_dates)
+  	.fail(function(message) {
+  		show_error_modal(message);
   	})
   }
 
-  function display_busy_dates(data){
-  	var busy_dates = data[0].busy_dates;
-  	console.log(busy_dates);
+  function set_busy_dates(data){
+  	$('.fc-busy').removeClass('fc-busy');
+  	if(data.length){
+  		busy_dates = data[0].busy_dates;
+  	}
   }
 
+  function show_error_modal(message){
+  	$('#error-modal #error-message').text(message);
+  	$('#error-modal').modal('show');
+  }
+
+  function collect_data(){
+  	return {
+  		start_date : selected_date.start.format('YYYY-MM-DD'),
+  		end_date : selected_date.end.format('YYYY-MM-DD'),
+  		team : $('#teams').val(),
+  		travaille : $('#travailles').val(),
+  	}
+  }
+
+  $('#affectation_form').on('submit',function(event) {
+  	// event.preventDefault();
+  	var data = collect_data();
+  	$.post('/path/to/file', data, function(data, textStatus, xhr) {
+  		/*optional stuff to do after success */
+  	});
+  });
 })(jQuery)
