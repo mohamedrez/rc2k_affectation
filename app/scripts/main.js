2bbe3ab982260ac5772534208869d8d2d60a6703
Hachimi <mohamed.rez@gmail.com>
2016-09-06 10:30:43 +0100

    fixed highlight of cells

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,45 @@
 (function($){
 
+	var selected_date = {};
   $('#calendar').fullCalendar({
 		weekends: false,
-		defaultView: 'basicWeek' 
+		defaultView: 'basicWeek',
+    eventClick: function(event) {
+			return false;
+    },
+   	dayClick: function (date, jsEvent, view) {
+      // $(jsEvent.target).toggleClass("fc-state-highlight");
+   		set_selected_date(date);
+   	} 
   });
+
+  function set_selected_date(date){
+  	if(!selected_date.start){
+  		selected_date.start = date;
+  		selected_date.end = date;
+  	}else{
+  		if(selected_date.start < date){
+  			selected_date.end = date;
+  		}else{
+  			selected_date.end = selected_date.start;
+  			selected_date.start = date;
+  		}
+  	}
+  }
+
+  function highlight_selected_date(){
+  	
+  	$('.fc-day').each(function(){
+  		var $this = $(this);
+  		var calendar_date = moment($this.data('date'));
+  		if(calendar_date.isBetween(selected_date.start, selected_date.end, 'days','[]')){
+  			$this.addClass('fc-selected');
+  		}else{
+  			$this.removeClass('fc-selected');
+  		}
+  	});
+  }
+
+  var colorise = setInterval(highlight_selected_date, 300);
 
 })(jQuery)
